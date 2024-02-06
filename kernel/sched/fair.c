0abbd93b18453d354a8d31c5fa6d457484ed18cf
David Vernet <void@manifault.com>
2024-02-05 22:39:20 -0600

    sched/fair: Do strict inequality check for busiest misfit task group
    In update_sd_pick_busiest(), when comparing two sched groups that are
    both of type group_misfit_task, we currently consider the new group as
    busier than the current busiest group even if the new group has the
    same misfit task load as the current busiest group. We can avoid some
    unnecessary writes if we instead only consider the newest group to be
    the busiest if it has a higher load than the current busiest. This
    matches the behavior of other group types where we compare load, such as
    two groups that are both overloaded.
    
    Let's update the group_misfit_task type comparison to also only update
    the busiest group in the event of strict inequality.
    
    Signed-off-by: David Vernet <void@manifault.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lore.kernel.org/r/20240206043921.850302-3-void@manifault.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10010,7 +10010,9 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 		 * If we have more than one misfit sg go with the biggest
 		 * misfit.
 		 */
-		return sgs->group_misfit_task_load > busiest->group_misfit_task_load;
+		if (sgs->group_misfit_task_load <= busiest->group_misfit_task_load)
+			return false;
+		break;
 
 	case group_fully_busy:
 		/*
