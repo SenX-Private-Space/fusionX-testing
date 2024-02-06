3767206d8659f295b669b3339ef788cbff9148fa
David Vernet <void@manifault.com>
2024-02-05 22:39:21 -0600

    sched/fair: Simplify the update_sd_pick_busiest() logic
    When comparing the current struct sched_group with the yet-busiest
    domain in update_sd_pick_busiest(), if the two groups have the same
    group type, we're currently doing a bit of unnecessary work for any
    group >= group_misfit_task. We're comparing the two groups, and then
    returning only if false (the group in question is not the busiest).
    
    Otherwise, we break out, do an extra unnecessary conditional check that's
    vacuously false for any group type > group_fully_busy, and then always
    return true.
    
    Let's just return directly in the switch statement instead. This doesn't
    change the size of vmlinux with llvm 17 (not surprising given that all
    of this is inlined in load_balance()), but it does shrink load_balance()
    by 88 bytes on x86. Given that it also improves readability, this seems
    worth doing.
    
    Signed-off-by: David Vernet <void@manifault.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lore.kernel.org/r/20240206043921.850302-4-void@manifault.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10010,9 +10010,7 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 		 * If we have more than one misfit sg go with the biggest
 		 * misfit.
 		 */
-		if (sgs->group_misfit_task_load <= busiest->group_misfit_task_load)
-			return false;
-		break;
+		return sgs->group_misfit_task_load > busiest->group_misfit_task_load;
 
 	case group_fully_busy:
 		/*
